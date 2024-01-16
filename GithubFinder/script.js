d7387e5cf903f74bc3e9dc75c6a7842d8b89e745
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-16 12:21:22 +0900

    feat(githubFinder): 프로필 정보 보이기 기능 추가, css 수정

--- GithubFinder/script.js
+++ GithubFinder/script.js
@@ -1,27 +1,40 @@
-function showImage() {
+async function fetchGitHubUserProfile() {
     var userId = document.getElementById("userId").value;
     var imageUrl = "https://github.com/" + userId + ".png";
     document.getElementById("profileImage").src = imageUrl;
+
+    var response = await fetch("https://api.github.com/users/" + userId);
+
+    if(response.status === 404){
+        alert('존재하지 않는 아이디입니다');
+        return;
+    }
+    let user = await response.json();
+
+    profileImage.style.visibility = "visible"; // 아이디 검색 후에는 이미지 보임
+
+    let profile = document.getElementById("profile");
+    // 사용자 이름과 회사 정보를 표시
+    profile.innerHTML = "이름: " + user.name + "<br/>" + "회사: " + (user.company ? user.company : "정보 없음");
 }
 
 async function showImageAndRepos() {
-    var userId = document.getElementById("userId").value;
-    var imageUrl = "https://github.com/" + userId + ".png";
-    document.getElementById("profileImage").src = imageUrl;
+    fetchGitHubUserProfile();
 
-    var response = await fetch("https://api.github.com/users/" + userId + "/repos");
-    var repos = await response.json();
+    let userId = document.getElementById("userId").value;
+    let response = await fetch("https://api.github.com/users/" + userId + "/repos");
+    let repos = await response.json();
 
-    var repoList = document.getElementById("list");
+    let repoList = document.getElementById("list");
     repoList.innerHTML = ""; // 리스트 초기화
 
     repos.forEach(function (repo) {
-        var repoLink = document.createElement("a");
+        let repoLink = document.createElement("a");
         repoLink.href = repo.html_url;
         repoLink.textContent = repo.name;
         repoList.appendChild(repoLink);
 
-        var breakLine = document.createElement("br");
+        let breakLine = document.createElement("br");
         repoList.appendChild(breakLine);
     });
 }
